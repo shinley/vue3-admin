d1c6090a16d7bc8d4d0e30ab7627619c8fd64883
liang82163 <liang82163@autoai.com>
2021-12-05 09:51:49 +0800

    完成登录ui

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,11 +2,11 @@
   <div class="login-container">
     <el-form class="login-form">
       <div class="title-container">
-        <h3>用户登录</h3>
+        <h3 class="title">用户登录</h3>
       </div>
       <!-- username -->
       <el-form-item>
-        <span class="sve-container">
+        <span class="svg-container">
           <el-icon>
             <avatar />
           </el-icon>
@@ -15,7 +15,7 @@
       </el-form-item>
       <!-- password -->
       <el-form-item>
-        <span class="sve-container">
+        <span class="svg-container">
           <el-icon>
             <avatar />
           </el-icon>
@@ -35,6 +35,75 @@
 <script setup>
 import { Avatar } from '@element-plus/icons'
 </script>
-<style lang="sss" scoped>
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
 
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background-color: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        caret-color: $cursor;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+    .title {
+      font-size: 16px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
 </style>
