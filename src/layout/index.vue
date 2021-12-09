536ac3171f6fa96e2402d5d46b8e93af06d61a17
shinley <chenxl4@autoai.com>
2021-12-09 17:20:51 +0800

    初步layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,40 @@
 <template>
-  <div class="">layout页面</div>
+  <div class="app-wrapper">
+    <!-- 左侧 menu -->
+    <sidebar class="sidebar-container" :style="{backgroundColor: variables.menuBg}"></sidebar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部 navbar -->
+        <navbar />
+      </div>
+    <!-- 内容区 -->
+    <app-main></app-main>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar'
+import AppMain from './components/AppMain.vue'
+import variables from '@/styles/variables.scss'
 </script>
 
 <style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
 
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
 </style>
