ae93ae777ae8980216acd083f227bd356ad86d3f
liang82163 <liang82163@autoai.com>
2021-12-11 15:56:43 +0800

    创建路由表结构

---
+++ src/views/user-info/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">用户信息</div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/user-manage/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">用户管理</div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+
+</style>
